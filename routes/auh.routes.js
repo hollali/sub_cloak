623677b46d099b42220f51d63701726b6b27788d
hollali <dheztinykartel@gmail.com>
2025-02-11 02:14:33 +0000

    `Added sign-in and sign-out routes to auth router and exported it`

--- routes/auh.routes.js
+++ routes/auh.routes.js
@@ -3,3 +3,7 @@ import { Router } from 'express';
 const authRouter = Router();
 
 authRouter.post('/sign-up', (reg, res) => res.send({title:'Sign Up'}));
+authRouter.post('/sign-in', (reg, res) => res.send({title:'Sign In'}));
+authRouter.post('/sign-out', (reg, res) => res.send({title:'Sign Out'}));
+
+export default authRouter;
