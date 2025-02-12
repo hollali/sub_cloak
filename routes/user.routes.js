9df4c01bc4f18230db878e0060f0fed8393c241c
hollali <dheztinykartel@gmail.com>
2025-02-12 02:23:46 +0000

    `Removed subscription routes and added back deleted user delete route`

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,3 +11,5 @@ userRouter.post("/" , (req,res) => res.send({title:'CREATE new user'}));
 userRouter.put("/:id" , (req,res) => res.send({title:'UPDATE users'}));
 
 userRouter.delete("/:id" , (req,res) => res.send({title:'DELETE user'}));
+
+export default userRouter;
