f6d7f7234c3955e5ba55626fdc918bdd4bafbd23
hollali <dheztinykartel@gmail.com>
2025-02-13 23:27:16 +0000

    chore: add comment to categorize middleware in app setup

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+//! Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
