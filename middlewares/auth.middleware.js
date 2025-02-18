64078bcfba8525930cff7acc5d38c3aea177454b
hollali <dheztinykartel@gmail.com>
2025-02-18 23:33:43 +0000

    fix: correct token extraction and improve error handling in authorization middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,32 +1,32 @@
-import { JWT_SECRET } from "../config/env.js";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env.js";
 import User from "../models/user.model.js";
 
 const authorize = async (req, res, next) => {
-    try {
-        let token;
+	try {
+		let token;
 
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-            token = req.headers.authorization.split('')[1];
-        }
+		if (
+			req.headers.authorization &&
+			req.headers.authorization.startsWith("Bearer")
+		) {
+			token = req.headers.authorization.split(" ")[1];
+		}
 
-        if (!token) return res.status(401).json({message:'Unauthorized'});
+		if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-        const decode = jwt.verify(token, JWT_SECRET);
+		const decoded = jwt.verify(token, JWT_SECRET);
 
-        const user = await User.findById(decode.userId);
+		const user = await User.findById(decoded.userId);
 
-        if(!user) return res.status(401).json({message: 'Unauthorized'});
+		if (!user) return res.status(401).json({ message: "Unauthorized" });
 
-        req.user = user;
-        next();
+		req.user = user;
 
-    } catch (error) {
-        res.status(401).json({
-            error: error.message,
-            message: 'Unauthorized',
-        });
-    }
-}
+		next();
+	} catch (error) {
+		res.status(401).json({ message: "Unauthorized", error: error.message });
+	}
+};
 
 export default authorize;
