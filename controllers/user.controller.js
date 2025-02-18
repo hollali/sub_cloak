3a70df5d6f293af6559be4d206486d653fd53047
hollali <dheztinykartel@gmail.com>
2025-02-18 13:46:20 +0000

    fix: update error message for user not found in getUser function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,7 +17,7 @@ export const getUser = async (req, res, next) => {
     try {
         const user = await User.findById(req.params.id).select("-password");
         if (!user) {
-            const error = new Error('User not found with id');
+            const error = new Error('User not found');
             error.statusCode = 404;
             throw error;
         }
